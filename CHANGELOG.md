32146f561422929c16200bbfff38a27ffa528eef
docktermj <michael@dockter.com>
2022-04-28 09:46:32 -0400

    #105 Prepare for version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 [markdownlint](https://dlaa.me/markdownlint/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.5.2] - 2022-04-21
+## [1.5.2] - 2022-04-28
 
 ### Changed in 1.5.2
 
--- redoer.py
+++ redoer.py
@@ -64,7 +64,7 @@ except:
 __all__ = []
 __version__ = "1.5.1"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2020-01-15'
-__updated__ = '2022-04-21'
+__updated__ = '2022-04-28'
 
 SENZING_PRODUCT_ID = "5010"  # See https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'
